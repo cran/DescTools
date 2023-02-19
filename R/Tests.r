1b0ef26f5e89e96cd4b91ab65af66234071d6037
Andri Signorell <andri@signorell.net>
2023-02-19 06:50:02 +0000

    version 0.99.48

--- R/Tests.r
+++ R/Tests.r
@@ -1982,7 +1982,7 @@ CramerVonMisesTest <- function (x) {
 
 
 #
-# AndersonDarlingTest <- function (x) {
+# AndersonDarlin<- <- function (x) {
 #
 #     DNAME <- deparse(substitute(x))
 #     x <- sort(x[complete.cases(x)])
@@ -2632,9 +2632,10 @@ MHChisqTest <- function(x, srow=1:nrow(x), scol=1:ncol(x)){
                  p.value = PVAL, method = METHOD, data.name = DNAME), class = "htest")
 }
 
+chisq.test
 
-
-GTest <- function(x, y = NULL, correct=c("none", "williams", "yates"), p = rep(1/length(x), length(x))) {
+GTest <- function(x, y = NULL, correct=c("none", "williams", "yates"), 
+                  p = rep(1/length(x), length(x)), rescale.p = FALSE) {
 
 
   # Log-likelihood tests of independence & goodness of fit
@@ -2757,12 +2758,25 @@ GTest <- function(x, y = NULL, correct=c("none", "williams", "yates"), p = rep(1
     }
   }
   else {
+    
+    
     # x is not a matrix, so we do Goodness of Fit
     METHOD <- "Log likelihood ratio (G-test) goodness of fit test"
-    if (length(x) == 1)
+    
+    if (length(dim(x)) > 2L) 
+      stop("invalid 'x'")
+    if (length(x) == 1L)
       stop("x must at least have 2 elements")
     if (length(x) != length(p))
-      stop("x and p must have the same number of elements")
+      stop("'x' and 'p' must have the same number of elements")
+    if (any(p < 0)) 
+      stop("probabilities must be non-negative.")
+    if (abs(sum(p) - 1) > sqrt(.Machine$double.eps)) {
+      if (rescale.p) 
+        p <- p/sum(p)
+      else stop("probabilities must sum to 1.")
+    }
+    
     E <- n * p
 
     if (correct=="yates"){ # Do Yates' correction
@@ -4935,12 +4949,18 @@ DunnettTest.default <- function (x, g, control = NULL
     diag(R) <- 1
 
     # store the given seed
-    old.seed <- .Random.seed
+    if(exists(".Random.seed")){
+      # .Random.seed might not exist when launched as background job
+      # so only store and reset if it exists 
+      old.seed <- .Random.seed
+    }
     set.seed(5)  # for getting consistent results every run
     qvt <- mvtnorm::qmvt((1 - (1 - conf.level)/2), df = N - k, sigma = R, tail = "lower.tail")$quantile
     
     # reset seed
-    .Random.seed <<- old.seed
+    if(exists("old.seed")){
+      .Random.seed <<- old.seed
+    }
     
     lower <- meandiffs - s * sqrt((1/fittedn) + (1/controln)) * qvt
     upper <- meandiffs + s * sqrt((1/fittedn) + (1/controln)) * qvt
